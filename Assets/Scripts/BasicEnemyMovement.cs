804f7571aaec50ae831157a1382c25fcc70a7956
Cheesecop <wakefielddonovan@gmail.com>
2025-04-09 21:59:08 -0700

    enemy movement wip

--- Assets/Scripts/BasicEnemyMovement.cs
+++ Assets/Scripts/BasicEnemyMovement.cs
@@ -6,28 +6,47 @@ using UnityEngine.EventSystems;
 public class BasicEnemyMovement : MonoBehaviour
 {
     public float speed = 10;
-    private new Rigidbody rigidbody;
     public Vector3 direction;
     
     // Start is called before the first frame update
     void Start()
     {
-       
+        Vector3 direction = Vector3.right;
+        transform.position += direction * speed * Time.deltaTime;
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+
+        Vector3 center = transform.position;
         
+
+        if(!Physics.Raycast(center, Vector3.right, out hit, 1.62f))
+        {
+            Vector3 direction = Vector3.right;
+            transform.position += direction * speed * Time.deltaTime;
+
+        }
+        if(!Physics.Raycast(center, Vector3.left, out hit, 1.62f))
+        {
+            Vector3 direction = Vector3.left ;
+            transform.position += direction * speed * Time.deltaTime;
+
+        }
     }
 
     private void MoveEnemy()
     {
         transform.position += direction * speed * Time.deltaTime;
-
+        
+            
        
 
     }
+
+    
+    
 }
