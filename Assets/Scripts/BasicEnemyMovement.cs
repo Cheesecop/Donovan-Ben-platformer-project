b37de328ff07489bd57fe0def25884b98a6ca3fc
Cheesecop <wakefielddonovan@gmail.com>
2025-04-14 12:40:26 -0700

    finished basic enemy movement

--- Assets/Scripts/BasicEnemyMovement.cs
+++ Assets/Scripts/BasicEnemyMovement.cs
@@ -7,35 +7,41 @@ public class BasicEnemyMovement : MonoBehaviour
 {
     public float speed = 10;
     public Vector3 direction;
-    
+    public float distance = 2.0f;
+    public bool rotation;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 direction = Vector3.right;
-        transform.position += direction * speed * Time.deltaTime;
+
+       if (rotation == true) 
+        {
+            direction = Vector3.forward;
+            
+        }
+       else 
+        {
+            direction = Vector3.left;
+        }
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-
-        Vector3 center = transform.position;
+        if (rotation == true)
+        {
+            
+            MoveEnemy();
+            TurnAroundLeft();
+        }
+        else if(rotation == false) 
+        {
+            MoveEnemy();
+            TurnAroundRight();
         
-
-        if(!Physics.Raycast(center, Vector3.right, out hit, 1.62f))
-        {
-            Vector3 direction = Vector3.right;
-            transform.position += direction * speed * Time.deltaTime;
-
         }
-        if(!Physics.Raycast(center, Vector3.left, out hit, 1.62f))
-        {
-            Vector3 direction = Vector3.left ;
-            transform.position += direction * speed * Time.deltaTime;
-
-        }
+        
     }
 
     private void MoveEnemy()
@@ -46,7 +52,63 @@ public class BasicEnemyMovement : MonoBehaviour
        
 
     }
+    private void TurnAroundLeft() 
+    {
+        if(OnGround() == false || WallForwardBack() == true)
+        {
+            direction = direction * -1;
+        } 
+    
+    }
+    private void TurnAroundRight() 
+    {
+        if(OnGround() == false || WallRightLeft() == true)
+        {
+            direction = direction * -1;
+        } 
+    
+    }
+    private bool OnGround() 
+    {
+        bool onGround = false;
 
-    
-    
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 2.0f)) 
+        {
+            onGround = true;
+        }
+        return onGround;
+    }
+    private bool WallRightLeft() 
+    {
+        bool wall = false;
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,Vector3.left, out hit, distance)) 
+        {
+            wall = true;
+        }
+        else if(Physics.Raycast(transform.position, Vector3.right, out hit, distance)) 
+        {
+            wall = true;
+        }
+        return wall;
+        
+    }
+    private bool WallForwardBack()
+    {
+        bool wall = false;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, distance))
+        {
+            wall = true;
+        }
+        else if (Physics.Raycast(transform.position, Vector3.back, out hit, distance))
+        {
+            wall = true;
+        }
+        return wall;
+
+    }
 }
