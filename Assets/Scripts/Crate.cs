7c0d154969b4c4dcc801b1c60275c39b494263f3
king-dyno <ben.klusnick@gmail.com>
2025-04-16 13:30:42 -0700

    Crate done

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class Crate : MonoBehaviour
 {
     public GameObject crateTop;
-    
+    public CharController charCtrl;
+    public GameObject wumpaFruit;
+
+    public int fruitCount;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.GetComponent<CharController>() && (gameObject.GetComponent<CharController>().attacking = true))
+        if (charCtrl.attacking == true)
         {
-            
+            //remove the crate
+            gameObject.SetActive(false);
+
+            //spawn the wumpa fruits
+            for (int i = fruitCount; i > 0; i--)
+            {
+                Instantiate(wumpaFruit, transform.position, transform.rotation);    
+
+            }
+
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        //remove the crate
+        gameObject.SetActive(false);
+
+        //spawn the wumpa fruits
+        for (int i = fruitCount; i > 0; i--)
+        {
+            Instantiate(wumpaFruit, transform.position, transform.rotation);
+
+        }
+
+    }
 }
